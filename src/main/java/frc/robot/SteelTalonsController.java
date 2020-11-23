f357c6911c4175b18cf90bd81d8857491ae2ba6d
GitHub-Jcb <jonnbenn2@gmail.com>
2020-11-23 11:09:17 -0600

    initial commit

---
+++ src/main/java/frc/robot/SteelTalonsController.java
@@ -0,0 +1,22 @@
+public class SteelTalonsController extends PWMVictorSPX{
+    
+
+    private boolean reverse;
+    private double bias; 
+
+    public SteelTalonsController(int channel, boolean reverse, double bias){
+        super(channel);
+        this.reverse = reverse;
+        this.bias = bias;
+        setInverted(reverse);
+    }
+
+    public void set( double speed, boolean reverse) {
+
+        if(reverse)
+            setInverted(!this.reverse);
+        else
+            setInverted(this.reverse);
+        super.set(speed * bias);
+    }
+}
---
+++ src/main/java/frc/robot/SteelTalonsControllerGroup.java
@@ -0,0 +1,21 @@
+public class SteelTalonsControllerGroup extends SpeedControllerGroup {
+
+   private double offset;
+   private boolean reverse;
+   
+    public SteelTalonsControllerGroup(double offset, boolean reverse, SpeedController speedController, SpeedController[] speedControllers) {
+        
+    
+        super(speedController, speedControllers);
+        this.offset = offset;
+        this.reverse = reverse;
+    }
+    
+    public void set(double speed, boolean reverse) {
+        if(reverse)
+            setInverted(!this.reverse);
+        else
+            setInverted(this.reverse);
+        super.set(speed + offset);
+    }
+}
